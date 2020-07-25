0ef3229998ccad2d6e9e5a9ed068fee9dfbd4559
mayralgr <ml17ft@gmail.com>
2020-07-25 16:00:20 -0500

    alias production

---
+++ src/main/java/com/example/urlShortener/UrlInputForm.java
@@ -0,0 +1,70 @@
+package com.example.urlShortener;
+
+import javax.validation.constraints.NotNull;
+
+import org.apache.commons.validator.routines.UrlValidator;
+
+public class UrlInputForm {
+
+	@NotNull
+	private String url;
+	private String alias;
+
+	public String getUrl() {
+		return this.url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getAlias() {
+		return this.alias;
+	}
+
+	public String toString() {
+		return "Url: " + this.url + ", Alias:" + this.alias ;
+	}
+
+	public boolean validateUrl() {
+		UrlValidator urlValid = new UrlValidator();
+		return urlValid.isValid(this.url) || urlValid.isValid("http://" + this.url) || urlValid.isValid("https://" + this.url) ;
+	}
+    public void generateAlias() {
+		if(this.isAGoogleUrl())
+		{
+			//  -   alias will have 5 characters length
+    		//  -   alias will use only alpha characters
+		}
+		else
+		{
+			if(isAYahooUrl())
+			{
+				// -   alias will have 7 characters length
+    			//-   alias will use alphanumeric characters
+			}
+			else{
+				// Any other url
+    			//-   alias will be based on the url itself by removing every special characters, vowels and numbers from it.
+				
+			}
+		}
+
+	}
+
+	private boolean isAGoogleUrl()
+	{
+		return this.url.contains("google");
+	}
+
+	private boolean isAYahooUrl()
+	{
+		return this.url.contains("yahoo");
+	}
+
+	private String generateRandomAlias(int size, int type)
+	{
+		return "";
+	}
+
+}
--- src/main/java/com/example/urlShortener/WebController.java
+++ src/main/java/com/example/urlShortener/WebController.java
@@ -10,45 +10,71 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class WebController implements WebMvcConfigurer {
-
-    List<Url> urlsGenerated = new ArrayList<Url>();
+    List<UrlInputForm> urlsGenerated = new ArrayList<UrlInputForm>();
     @Override
 	public void addViewControllers(ViewControllerRegistry registry) {
 		registry.addViewController("/results").setViewName("results");
 	}
 
 	@GetMapping("/")
-	public ModelAndView showForm(Url url) {
+	public ModelAndView showForm(UrlInputForm urlForm) {
 		ModelAndView mv = new ModelAndView();
         mv.addObject("url", "hola");
         mv.setViewName("form");
         return mv;
 	}
 
+    /**
+     * Where 
+     * url : string for json post
+     * urlForm: url from form interface
+     * 
+     */
 	@PostMapping("/")
-	public ModelAndView checkUrl(@Valid Url url, Model model, BindingResult bindingResult) {
+    public ModelAndView checkUrl(// @RequestBody String url, 
+                                @Valid UrlInputForm urlForm, 
+                                Model model, 
+                                BindingResult bindingResult) {
+        // if it comes from post and not the interface, the object is set
+
+        /*System.out.println(url);
+        if (urlForm.getUrl() == null)
+        {
+            urlForm = new UrlInputForm();
+            urlForm.setUrl(url);
+        }*/
+
 		if (bindingResult.hasErrors()) {
 			ModelAndView mv = new ModelAndView("form");
-            mv.addObject("url", url.getUrl());
+            mv.addObject("url", urlForm.getUrl());
             mv.setViewName("form");
             return mv;
         }
-        System.out.println(url.validateUrl());
-        if(url.validateUrl()){
+        
+        System.out.println(urlForm.getUrl());
+        System.out.println(urlForm.validateUrl());
+        if(urlForm.validateUrl()){
             // is a valid url, an alias is generated
-            url.generateAlias();
-            System.out.println(url.getAlias());
-            urlsGenerated.add(url);
-            
+            urlForm.generateAlias();
+            System.out.println(urlForm.getAlias());
+            // verification that does not exist in the list
+            if(!urlsGenerated.stream().anyMatch(source -> source.getUrl() == urlForm.getUrl()))
+            {
+                urlsGenerated.add(urlForm);
+                System.out.println("success");
+                System.out.println(urlsGenerated.toString());
+            }
         }
         ModelAndView mv = new ModelAndView("results");
-        mv.addObject("url", url.getUrl());
+        mv.addObject("url", urlForm.getUrl());
         mv.setViewName("results");
         return mv;
     }
